a4533e86ff351226f8539f1fae5167be9292d63c
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-06-02 23:40:07 +0200

    Add tooltip

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -6,6 +6,7 @@ import App from "@/App";
 
 import NotFoundPage from "@/pages/404/404";
 import Contact from "@/pages/contact/Contact";
+import {TooltipProvider} from "@/components/ui/tooltip.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -14,10 +15,14 @@ export const router = createBrowserRouter([
     },
     {
         path: "/contact",
-        element: <Contact />,
+        element: <TooltipProvider>
+                    <Contact />
+                 </TooltipProvider>,
     },
     {
         path: "*",
-        element: <NotFoundPage />,
+        element: <TooltipProvider>
+                    <NotFoundPage />
+                 </TooltipProvider>,
     },
 ]);
