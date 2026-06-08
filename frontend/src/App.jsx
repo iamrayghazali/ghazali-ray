30e9b55ce269f86c0dbfea1581e52d6f291b695e
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-06-08 23:37:23 +0200

    App.jsx rewrite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,70 @@
 import './App.css'
-import {ThemeToggle} from "@/components/custom/ThemeToggle.jsx";
 import Hero from "@/pages/home/Hero.jsx";
 import Navbar from "@/components/custom/Navbar.jsx";
 import {LightRays} from "@/components/ui/light-rays.jsx";
+import SkipButton from "@/components/custom/SkipButton.jsx";
+import Bento from "@/components/custom/Bento.jsx";
+import {TooltipProvider} from "@/components/ui/tooltip.jsx";
+import ProjectsScroll from "@/pages/home/Projectscroll.jsx";
+import ScrollTextReveal from "@/components/custom/ScrollTextReveal.jsx";
+import {FaCss3Alt, FaFigma, FaJava, FaJs, FaReact} from "react-icons/fa";
+import React from "react";
+import {SiExpress, SiMongodb, SiMysql, SiPostgresql, SiTailwindcss, SiTypescript} from "react-icons/si";
 
 function App() {
 
-  return (
-    <>
-      <LightRays />
-        <Navbar />
-        <Hero></Hero>
-    </>
-  )
+    const techList = [
+        {
+            label: "The Plan",
+            title: "Design.",
+            description: "I create websites from A to Z. Batteries included.",
+            logos: [
+                <FaFigma className="text-muted-foreground" size={50}/>
+            ]
+        },
+        {
+            label: "The Visuals",
+            title: "Front End.",
+            description: "Although I am a Full stack software engineer, I like Front-end work more.",
+            logos: [
+                <FaReact className="text-muted-foreground" size={50}/>,
+                <FaJs className="text-muted-foreground" size={50}/>,
+                <SiTypescript className="text-muted-foreground" size={50}/>,
+                <FaCss3Alt className="text-muted-foreground" size={50}/>,
+                <SiTailwindcss className="text-muted-foreground" size={50}/>,
+            ]
+        },
+        {
+            label: "The Brain",
+            title: "Back End.",
+            description: "From building APIs to databases.",
+            logos: [
+                <FaJava className="text-muted-foreground" size={50}/>,
+                <SiExpress className="text-muted-foreground" size={50}/>,
+                <SiMysql className="text-muted-foreground" size={50}/>,
+                <SiMongodb className="text-muted-foreground" size={50}/>,
+                <SiPostgresql className="text-muted-foreground" size={50}/>
+            ]
+        },
+    ]
+    return (
+        <TooltipProvider>
+            <LightRays/>
+            <SkipButton/>
+            <div className="flex justify-center items-center">
+                <Navbar/>
+            </div>
+            <Hero/>
+            {techList.map((tech, index) => (
+                <ScrollTextReveal key={index} label={tech.label} title={tech.title} description={tech.description} logos={tech.logos} first={index === 0} />
+            ))}
+            <ProjectsScroll/>
+            <div className="min-h-screen bg-red-400"></div>
+            <div className="min-h-screen bg-amber-200"></div>
+            <div className="min-h-screen bg-green-200"></div>
+            <Bento></Bento>
+        </TooltipProvider>
+    )
 }
 
 export default App
