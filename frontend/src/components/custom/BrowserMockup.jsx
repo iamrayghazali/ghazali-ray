9d7b378910890bacbc87f7c0734a328a4a7dee75
iamrayghazali <ghazali.raydan@gmail.com>
2026-06-12 17:48:20 +0200

    Update mockup

--- frontend/src/components/custom/BrowserMockup.jsx
+++ frontend/src/components/custom/BrowserMockup.jsx
@@ -1,17 +1,43 @@
+import {IoBookOutline, IoLockClosed, IoRefreshOutline} from "react-icons/io5";
+import {LuPanelLeftDashed} from "react-icons/lu";
+import {MdChevronLeft, MdChevronRight} from "react-icons/md";
+import { IoShieldHalfSharp } from "react-icons/io5";
+import {GoShare} from "react-icons/go";
+import {FiPlus} from "react-icons/fi";
+import {IoIosRefresh} from "react-icons/io";
+
 export default function BrowserMockup({ imageSrc, url }) {
     return (
         <div className="overflow-hidden rounded-xl border border-border bg-white shadow-2xl">
             {/* Browser Top Bar */}
-            <div className="flex items-center gap-1 border-b border-border px-4 py-3">
-                <div className="h-2 w-2 rounded-full bg-red-500/50" />
-                <div className="h-2 w-2 rounded-full bg-yellow-500/50" />
-                <div className="h-2 w-2 rounded-full bg-green-500/50" />
 
-                <div className="ml-4 flex-1">
-                    <div className="truncate rounded-md bg-gray-300 px-3 py-1 text-center text-[6px] md:text-[8px] text-black">
-                        {url}
+
+            <div className="flex items-center gap-1 border-b border-border px-4 py-2 bg-gray-100">
+                <div className="h-2 w-2 rounded-full bg-gray-300" />
+                <div className="h-2 w-2 rounded-full bg-gray-300" />
+                <div className="h-2 w-2 rounded-full bg-gray-300" />
+                <LuPanelLeftDashed size={10} className="text-gray-400 ml-2"/>
+                <MdChevronLeft size={12} className="text-gray-400"/>
+                <MdChevronRight size={12} className="text-gray-400"/>
+                <IoShieldHalfSharp  size={10} className="text-gray-400 ml-6 mr-1"/>
+                <div className=" flex-1">
+                    <div className="relative flex items-center truncate rounded-sm bg-gray-300  py-1 text-center text-[6px] md:text-[8px] text-black">
+                        <div className="w-2">
+
+                        </div>
+                        <div className="absolute left-1/2 -translate-x-1/2 flex items-center">
+                            <IoLockClosed className=""/>
+                            {url}
+                        </div>
+                        <IoIosRefresh  className="ml-auto mr-4"/>
                     </div>
                 </div>
+
+                <GoShare size={10} className="text-gray-400 ml-6 mr-1"/>
+                <FiPlus size={10} className="text-gray-400 mr-1"/>
+
+
+
             </div>
 
             {/* Screenshot */}
