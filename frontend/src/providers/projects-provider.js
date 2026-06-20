c4d8adbcce52965059efeb10e94a87157988ba15
iamrayghazali <ghazali.raydan@gmail.com>
2026-06-20 15:53:56 +0200

    Update projects image

---
+++ frontend/src/providers/projects-provider.js
@@ -0,0 +1,34 @@
+export const PROJECTS = [
+    {
+        id: 1,
+        title: "But Make It Smart",
+        description: "A webshop that sells NFC-based smart items like business cards.",
+        url: "https://github.com/iamrayghazali/ButMakeItSmart",
+        src: "/assets/project-showcase/butmakeitsmart-desktop-2.png",
+        tag: "Frontend",
+    },
+    {
+        id: 2,
+        title: "Camera101",
+        description: "A paid service to learn interactively how to use multiple camera types, even your iPhone. Simulators included.",
+        url: "https://github.com/iamrayghazali/camera101",
+        src: "/assets/project-showcase/camera101-desktop.png",
+        tag: "Fullstack",
+    },
+    {
+        id: 3,
+        title: "VIPRENT",
+        description: "A luxury and sport car rental company based in Budapest.",
+        url: "https://github.com/iamrayghazali/VIPRENT",
+        src: "/assets/project-showcase/viprent-desktop-cars.png",
+        tag: "Fullstack",
+    },
+    {
+        id: 4,
+        title: "Vibe Atlas",
+        description: "A smart travel suggestor using user preferences and AI to generate curated destination suggestions.",
+        url: "https://github.com/iamrayghazali/VibeAtlas2",
+        src: "/assets/project-showcase/vibeatlas-desktop.png",
+        tag: "Fullstack",
+    },
+];
