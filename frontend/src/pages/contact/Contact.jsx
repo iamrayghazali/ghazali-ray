137a5fb1fe1cb3db1bf538efe6afe5a66a5b5bf6
iamrayghazali <ghazali.raydan@gmail.com>
2026-06-22 00:30:08 +0200

    Add copy functionality to email address

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,10 +1,26 @@
 import Navbar from "@/components/custom/Navbar.jsx";
 import {DiaTextReveal} from "@/components/ui/dia-text-reveal.jsx";
 import ContactForm from "@/components/custom/ContactForm.jsx";
+import {Separator} from "@/components/ui/separator.jsx";
+import { TbCopy } from "react-icons/tb";
+import {toast} from "sonner";
 
 
 
 export default function Contact() {
+
+    const email = "ghazali.raydan@gmail.com";
+
+
+    const copyText = async () => {
+        try {
+            await navigator.clipboard.writeText(email);
+            toast.success("Copied!", { position: "bottom-right" });
+        } catch (err) {
+            toast.error("Couldn't copy — try manually selecting it", { position: "bottom-right" });
+        }
+    };
+
     return (
         <section className="min-h-screen">
             <Navbar />
@@ -16,6 +32,15 @@ export default function Contact() {
                     colors={["#f97316"]}
                 />
                 <ContactForm />
+                <Separator className="w-2/3 mt-5" />
+                <div className="flex flex-col items-center justify-center w-full mt-10">
+                    <p className="mb-4">Or copy my email</p>
+                    <div className="flex items-center justify-center gap-2" onClick={copyText}>
+                        <p className="text-sm text-gray-500">{email}</p>
+                        <Separator className="h-6" orientation="vertical"/>
+                        <TbCopy className=""></TbCopy>
+                    </div>
+                </div>
             </div>
         </section>
     );
