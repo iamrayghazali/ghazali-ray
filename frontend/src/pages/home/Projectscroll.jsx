c0e6abef2a48f24d7eb5895124ec28a70b30eea0
iamrayghazali <ghazali.raydan@gmail.com>
2026-06-22 00:29:02 +0200

    Change to import

--- frontend/src/pages/home/Projectscroll.jsx
+++ frontend/src/pages/home/Projectscroll.jsx
@@ -2,41 +2,7 @@ import { useRef } from "react";
 import { motion, useScroll, useTransform } from "motion/react";
 import { cn } from "@/lib/utils";
 import BrowserMockup from "@/components/custom/BrowserMockup.jsx";
-
-const PROJECTS = [
-    {
-        id: 1,
-        title: "But Make It Smart",
-        description: "A webshop that sells NFC-based smart items like business cards.",
-        url: "https://github.com/iamrayghazali/ButMakeItSmart",
-        src: "/assets/project-showcase/butmakeitsmart-desktop.png",
-        tag: "Frontend",
-    },
-    {
-        id: 2,
-        title: "Camera101",
-        description: "A paid service to learn interactively how to use multiple camera types, even your iPhone. Simulators included.",
-        url: "https://github.com/iamrayghazali/camera101",
-        src: "/assets/project-showcase/camera101-desktop.png",
-        tag: "Fullstack",
-    },
-    {
-        id: 3,
-        title: "VIPRENT",
-        description: "A luxury and sport car rental company based in Budapest.",
-        url: "https://github.com/iamrayghazali/VIPRENT",
-        src: "/assets/project-showcase/viprent-desktop-cars.png",
-        tag: "Fullstack",
-    },
-    {
-        id: 4,
-        title: "Vibe Atlas",
-        description: "A smart travel suggestor using user preferences and AI to generate curated destination suggestions.",
-        url: "https://github.com/iamrayghazali/VibeAtlas2",
-        src: "/assets/project-showcase/vibeatlas-desktop.png",
-        tag: "Fullstack",
-    },
-];
+import { PROJECTS } from "@/providers/projects-provider.js";
 
 // desktop: (n-1) * (cardVw + gapVw) = 3 * (50 + 2) = 156
 // mobile:  (n-1) * (cardVw + gapVw) = 3 * (82 + 2) = 252
